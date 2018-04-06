6d411a56556569abe62fb2b8833d446294b34bf1
zeetum <timothy.blackburn@education.wa.edu.au>
2018-04-06 14:12:25 +0800

    Update get_board_details.py

--- get_board_details.py
+++ get_board_details.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # https://www.activexperts.com/admin/scripts/wmi/python/
+# https://msdn.microsoft.com/en-us/library/aa394589(v=vs.85).aspx?cs-save-lang=1&cs-lang=vb#code-snippet-1
 from ldap3 import Server, Connection
 import win32com.client
 
