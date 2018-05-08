e67fd4347ce4a4ade1144616d42ffff0d5e3806b
zeetum <timothy.blackburn@education.wa.edu.au>
2018-05-08 11:04:34 +0800

    Update get_computer_details.py

--- get_computer_details.py
+++ get_computer_details.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 
-# Getting wmic working: https://www.si458.co.uk/?p=134
-# wmic -U INDIGO/<user>%<pass> //MS4CCC6AE71035 "SELECT Version FROM Win32_ComputerSystemProduct"
-# wmic -U INDIGO/<user>%<pass> //MS4CCC6AE71035 "SELECT UserName FROM Win32_ComputerSystem"
-
 # https://pypi.python.org/pypi/wmicq/1.0.0
 # https://www.activexperts.com/admin/scripts/wmi/python/
 # https://msdn.microsoft.com/en-us/library/aa394589(v=vs.85).aspx?cs-save-lang=1&cs-lang=vb#code-snippet-1
 from ldap3 import Server, Connection
+import wmi_client_wrapper as wmi
+import sys
 import os
 
 server_address = "e5070s01sv001.indigo.schools.internal"
 domain = "indigo"
-user = "<username>"
-password = "<password>"
-OU_csv = "./OUs.csv"
-
+user = "e4088746"
+password = "Wonderful4"
 
 # Contacts LDAP to get a list of hostnames that are in the specified OU
 def get_ou_computers(OU):
@@ -23,7 +19,7 @@ def get_ou_computers(OU):
     server = Server(server_address)
     with Connection(server, user=domain + "\\" + user, password=password) as conn:
 
-        BaseDN = OU + "OU=Desktops,OU=School Managed,OU=Computers,OU=E5070S01,OU=Schools,DC=indigo,DC=schools,DC=internal"
+        BaseDN = OU + ",OU=Desktops,OU=School Managed,OU=Computers,OU=E5070S01,OU=Schools,DC=indigo,DC=schools,DC=internal"
         Filter = "(objectCategory=computer)"
         conn.search(BaseDN,Filter)
 
@@ -35,8 +31,7 @@ def get_ou_computers(OU):
 
 # Ping computers to see if they're online
 def get_online_computers(hostnames):
-    
-    online = ()
+    online = tuple()
     for hostname in hostnames:
         if (os.system("ping -c 1 " + hostname + " > /dev/null 2>&1") == 0):
             online += (hostname,)
@@ -44,28 +39,30 @@ def get_online_computers(hostnames):
     return online
 
 # Contacts WIM to get a list of boardnames associated with the supplied hostnames
-def get_computer_details(hostnames):
+def get_board_details(hostnames):
     
-    details = ()
+    boards = ()
     for hostname in hostnames:
         output = os.popen("/bin/wmic --user=" + domain + "/" + user +"%" + password + " //" + hostname + " \"SELECT Version FROM Win32_ComputerSystemProduct\"").read()
-        details += (output.split("|")[2][8:],)
+        boards += (output.split("|")[2][8:],)
 
-    return details 
+    return boards 
 
+# Usage:
+#     get_board_details "OU=Room 13,OU=Block H"
+def main():
 
-# Opens a CSV with the OUs you'd like computer details from
-with open(OU_csv) as csv_read:
-    #OUs = list(csv.reader(csv_read))
-    OUs = ("OU=Room 13,OU=Block H,",)
+    for OU in range(1,len(sys.argv)):
 
-    for OU in OUs:
-
-        hostnames = get_ou_computers(OU)
+        hostnames = get_ou_computers(sys.argv[OU])
         hostnames = get_online_computers(hostnames)
-        details = get_computer_details(hostnames)
+        boardnames = get_board_details(hostnames)
 
-        print(OU)
-        for h, b in zip(hostnames, details):
+        print(sys.argv[OU])
+        print(str(len(hostnames)) + " computers")
+        for h, b in zip(hostnames, boardnames):
             print("hostname: " + h + " model: " + b)
 
+
+
+main()
