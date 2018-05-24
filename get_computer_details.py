d65ce3963a01816759000b1536afbe0e792aaa69
zeetum <timothy.blackburn@education.wa.edu.au>
2018-05-24 16:01:17 +0800

    Update get_computer_details.py

--- get_computer_details.py
+++ get_computer_details.py
@@ -36,7 +36,7 @@ def get_ou_computers(OU):
 # Ping computers to see if they're online
 def get_online_computers(hostnames):
     
-    online = tuple()
+    online = ()
     for hostname in hostnames:
         if (os.system("ping -c 1 " + hostname + " > /dev/null 2>&1") == 0):
             online += (hostname,)
